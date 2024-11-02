7e86c780e81644b03a00fc30c3eef9cf53857dba
skylark1894 <123784653+skylark1894@users.noreply.github.com>
2024-11-02 20:09:46 +0700

    Add files via upload

---
+++ Kelipatan.java
@@ -0,0 +1,21 @@
+/*
+ * Nama:Moh.arif prasetyo
+ * Nim : 23215043
+ * Prodi : Teknik Informatika
+ */
+package pertemuan5;
+
+/**
+ *
+ * @author user
+ */
+public class Kelipatan {
+    
+    public static void main(String[] args) {
+        System.out.println("Bilangan kelipatan 3.5 dari 3.5 sampai 35:");
+        
+        for (double i = 3.5; i <= 35; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
